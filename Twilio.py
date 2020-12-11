1b3ec98ef9e4ee63a7f15ee4be1d94f60f6ce7e3
rhauser15 <38057304+rhauser15@users.noreply.github.com>
2020-12-11 09:46:40 -0700

    gitignore
    gitignore changes

--- Twilio.py
+++ Twilio.py
@@ -51,19 +51,26 @@ le2 = pd.DataFrame({
 
 #Interactive checkbox
 if st.button('Sattelite Down Test'):
-    message = client.messages.create(
-        to="+19199498424",
-        from_="+19048228670",
-        body="alert")
-    execution = client.studio \
-        .flows('FW58181827a9c369058b7f9f1310cdcb34') \
-        .executions \
-        .create(to='+19199498424', from_='+19048228670')
+    # #SMS trigger
+    # message = client.messages.create(
+    #     to="+19199498424",
+    #     from_="+19048228670",
+    #     body="alert")
+    # #Twilio Studio trigger
+    # execution = client.studio \
+    #     .flows('FW58181827a9c369058b7f9f1310cdcb34') \
+    #     .executions \
+    #     .create(to='+19199498424', from_='+19048228670')
+    call = client.calls.create(
+        machine_detection='DetectMessageEnd',
+        twiml='<Response><Pause length="1"/><Say> Hello Jordan. We are watching you.</Say> <Pause length="1"/> <Say>See you soon.</Say> <Pause length="1"/> <Say> Goodbye.</Say></Response>',
+        to='+12146089086',
+        from_='+19048228670'
+    )
 
-    print(execution.sid)
-
-
-    print(message.sid)
+    print(call.sid)
+#    print(execution.sid)
+ #   print(message.sid)
 
 
     st.write('1 Satellite(s) are down. Roger has been notified')
